3f1c1a65635165fcc719c577d558784e0df7d34c
KYC <go95305@naver.com>
2021-02-05 01:11:38 +0900

    Websocket chating by jwt security

--- src/main/java/com/websocket/chat/controller/ChatRoomController.java
+++ src/main/java/com/websocket/chat/controller/ChatRoomController.java
@@ -1,8 +1,12 @@
 package com.websocket.chat.controller;
 
 import com.websocket.chat.dto.ChatRoom;
+import com.websocket.chat.dto.LoginInfo;
 import com.websocket.chat.repository.ChatRoomRepository;
+import com.websocket.chat.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,7 @@ public class ChatRoomController {
 
     private final ChatRoomRepository chatRoomRepository;
 
+    private final JwtTokenProvider jwtTokenProvider;
     // 채팅 리스트 화면
     @GetMapping("/room")
     public String rooms(Model model) {
@@ -44,5 +49,13 @@ public class ChatRoomController {
     @ResponseBody
     public ChatRoom roomInfo(@PathVariable String roomId) {
         return chatRoomRepository.findRoomById(roomId);
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    public LoginInfo getUserInfo() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        return LoginInfo.builder().name(name).token(jwtTokenProvider.generateToken(name)).build();
     }
 }
