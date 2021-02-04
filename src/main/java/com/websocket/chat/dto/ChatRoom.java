ae6be9fb6c903dd2c1689b353ef47e37b164cc19
koyuchang <go95305@naver.com>
2021-02-04 09:13:21 +0900

    Websocket implementation by Redis Multi chating service by help with Redis

--- src/main/java/com/websocket/chat/dto/ChatRoom.java
+++ src/main/java/com/websocket/chat/dto/ChatRoom.java
@@ -2,11 +2,16 @@ package com.websocket.chat.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
 
