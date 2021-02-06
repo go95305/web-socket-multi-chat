ee04f2e5fea92a2f408e69d8e6ff075f16cc357e
KYC <go95305@naver.com>
2021-02-06 09:58:48 +0900

    Exit and UserCount notice

--- src/main/java/com/websocket/chat/dto/ChatRoom.java
+++ src/main/java/com/websocket/chat/dto/ChatRoom.java
@@ -12,8 +12,9 @@ public class ChatRoom implements Serializable {
 
     private static final long serialVersionUID = 6494678977089006639L;
 
-    private String roomId;
-    private String name;
+    private String roomId; //채팅방 ID
+    private String name; //채팅방 이름
+    private long userCount; // 채팅방 인원수
 
     public static ChatRoom create(String name) {
         ChatRoom chatRoom = new ChatRoom();
